c242f0c935c0535a072371883d380509fc059c5e
Derrick Truong <dtruong169@gmail.com>
2024-06-17 23:15:46 -0700

    contactform-authorbio-calendar

---
+++ src/app/contactUs.tsx
@@ -0,0 +1,56 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactUs = () => {
+    const form = useRef(null); // Explicitly initialize useRef with null
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        if (form.current) {
+            emailjs
+                .sendForm('service_4n7fkjl', 'template_2rtq1s9', form.current, 'YOUR_PUBLIC_KEY')
+                .then(
+                    () => {
+                        console.log('SUCCESS!');
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    }
+                );
+        } else {
+            console.log('Form reference is undefined.');
+        }
+    };
+
+    // <form ref={form} onSubmit={sendEmail}>
+    //     <label>Name</label>
+    //     <input type="text" name="user_name" required />
+    //     <label>Email</label>
+    //     <input type="email" name="user_email" required />
+    //     <label>Message</label>
+    //     <textarea name="message" required />
+    //     <input type="submit" value="Send" />
+    // </form>
+    // return (
+    //     <>
+    //     <section>
+    //         <div className='contactContainer'>
+    //             <h2 className='--text-center'>Contact Me
+    //             </h2>
+    //             <form className='--form-control --card'>
+    //                 <input type='text' placeholder='Full Name'
+    //                     name='user_name' required />
+    //                 <input type='text' placeholder='Email'
+    //                     name='user_email' required />
+    //                 <textarea name='message' cols='30' rows='10'>
+    //                 </textarea>
+    //                 <button type='submit' className='--btn-primary'>Send Message</button>
+    //             </form>
+    //         </div>
+    //     </section>
+    //     </>
+    // );
+};
+
+export default ContactUs;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,23 @@
 "use client";
 
 import { useState } from 'react';
+import { useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import HeadImage from '../assets/Warren_headshot.jpg'; // Adjust the path based on the location of your image
 import Algebra from '../assets/algebra.jpg';
 import Calculus from '../assets/calculus.jpg';
 import Geometry from '../assets/geometry.jpg';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import ContactUs from './contactUs';
 
 export default function Home() {
   const [popupImageSrc, setPopupImageSrc] = useState<string | null>(null);
+  const [popupContact, setPopupContact] = useState<string | null>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleImageClick = (src: string) => {
     setPopupImageSrc(src);
@@ -19,6 +27,33 @@ export default function Home() {
   const handleClosePopup = () => {
     setPopupImageSrc(null);
   };
+ 
+  const handleContactFormClick = () => {
+    setPopupContact('contactForm');
+  };
+
+  const handleCloseContact = () => {
+    setPopupContact(null);
+  };
+
+  const toggleParagraph = () => {
+    setIsExpanded(!isExpanded);
+  };
+  
+  const truncatedText = "Hello! I am Warren Fernandez, a dedicated mathematics tutor with a deep-rooted passion for teaching. My educational journey began in San Francisco, where I attended Edison Elementary, followed by Ben Franklin Middle School and Jefferson High School in Daly City. I went on to earn my Master’s degree in Mathematics from Cal Poly San Luis Obispo. Since 2000, I have been teaching high school mathematics at a K - 12 school in the East Bay.I have also had the privilege of teaching at Cal State East Bay and the College of San Mateo.Throughout my career, I have been fortunate to achieve a 95 % pass rate among my AP Calculus students on the AP exam.I have also been honored to serve as a graduation commencement speaker and received a yearbook dedication. Beyond my professional life, I am a husband and father who loves to travel and maintain an active lifestyle.  My teaching philosophy is centered on making math accessible and enjoyable for all students. I have extensive experience working with students from diverse backgrounds and different learning styles";
+  const fullText = "Hello! I am Warren Fernandes, a dedicated mathematics tutor with a deep-rooted passion for teaching. My educational journey began in San Francisco, where I attended Edison Elementary, followed by Ben Franklin Middle School and Jefferson High School in Daly City. I went on to earn my Master’s degree in Mathematics from Cal Poly San Luis Obispo. Since 2000, I have been teaching high school mathematics at a K-12 school in the East Bay. I have also had the privilege of teaching at Cal State East Bay and the College of San Mateo. Throughout my career, I have been fortunate to achieve a 95% pass rate among my AP Calculus students on the AP exam. I have also been honored to serve as a graduation commencement speaker and received a yearbook dedication. Beyond my professional life, I am a husband and father who loves to travel and maintain an active lifestyle. My teaching philosophy is centered on making math accessible and enjoyable for all students. I have extensive experience working with students from diverse backgrounds and different learning styles, and I am passionate about helping each student reach their full potential. I am available to tutor Algebra 1 through AP Calculus, as well as standardized tests such as the ACT and SAT I/II. Tutoring sessions can be conducted via ZOOM or in person, depending on your preference. Whether you're struggling with basic concepts or aiming to master advanced topics, I am here to provide personalized guidance and support to help you succeed.";
+
+  const businessHours = [
+    {
+      // days of week. an array of zero-based day of week integers (0=Sunday)
+      daysOfWeek: [1, 2, 3, 4], // Monday - Thursday
+
+      startTime: '10:00', // a start time (10am in this example)
+      endTime: '18:00', // an end time (6pm in this example)
+    }
+  ];
+  
+  
 
   return (
     <>
@@ -40,7 +75,7 @@ export default function Home() {
           <Image alt="Head" src={HeadImage} className="head" />
           <div className="authorBody">
             <div className="authorTitle">
-              <h1 className="authorName">Warren Fernandez</h1>
+              <h1 className="authorName">Warren Fernandes</h1>
               <div className="topicsOuter">
                 <div className="topics">Algebra</div>
                 <div className="topics">Geometry</div>
@@ -51,14 +86,41 @@ export default function Home() {
             <h2 className="jobTitle">Mathematics Tutor</h2>
             <div className="outerDescription">
               <p className="description">
-                Hello! I am Warren Fernandez, a dedicated mathematics tutor with a deep-rooted passion for teaching. My educational journey began in San Francisco, where I attended Edison Elementary, followed by Ben Franklin Middle School and Jefferson High School in Daly City. I went on to earn my Master’s degree in Mathematics from Cal Poly San Luis Obispo.
-
-                Since 2000, I have been teaching high school mathematics at a K-12 school in the East Bay. I have also had the privilege of teaching at Cal State East Bay and the College of San Mateo. Throughout my career, I have been fortunate to achieve a 95% pass rate among my AP Calculus students on the AP exam. I have also been honored to serve as a graduation commencement speaker and received a yearbook dedication.
-
-                Beyond my professional life, I am a husband and father who loves to travel and maintain an active lifestyle. My teaching philosophy is centered on making math accessible and enjoyable for all students. I have extensive experience working with students from diverse backgrounds and different learning styles, and I am passionate about helping each student reach their full potential.
-
-                I am available to tutor Algebra 1 through AP Calculus, as well as standardized tests such as the ACT and SAT I/II. Tutoring sessions can be conducted via ZOOM or in person, depending on your preference. Whether you're struggling with basic concepts or aiming to master advanced topics, I am here to provide personalized guidance and support to help you succeed.
+                {isExpanded ? fullText:truncatedText}
+                {!isExpanded && (
+                  <span className="expandText" onClick={toggleParagraph}>
+                    ...more
+                  </span>
+                )}
               </p>
+            </div>
+          </div>
+          <div className='calendarOuter'>
+            <FullCalendar
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              initialView={'dayGridMonth'}
+              customButtons={
+                {
+                  myCustomButton: {
+                    text: 'Availability'
+                  }
+                }
+              }
+              headerToolbar={
+                {
+                  start: 'myCustomButton',
+                  center: 'title',
+                  end: 'prev,next'
+                }
+              }
+              businessHours={businessHours}
+              height='45vh'
+            />
+            <div>
+            <button  onClick={handleContactFormClick} className='contactUsOuter'>
+              Contact Me
+              {/* <ContactUs/> */}
+            </button>   
             </div>
           </div>
         </div>
@@ -71,11 +133,11 @@ export default function Home() {
             </div>
             <div className="studyPic" onClick={() => handleImageClick(Geometry.src)}>
               <Image src={Geometry} alt="Geometry" className="studyImage" />
-              <div className='studyPicTitles2'>Geometry</div>    
+              <div className='studyPicTitles2'>Geometry</div>
             </div>
             <div className="studyPic" onClick={() => handleImageClick(Calculus.src)}>
               <Image src={Calculus} alt="Calculus" className="studyImage" />
-              <div className='studyPicTitles3'>Calculus</div>    
+              <div className='studyPicTitles3'>Calculus</div>
             </div>
           </div>
         </div>
@@ -85,6 +147,14 @@ export default function Home() {
             <Image src={popupImageSrc} alt="Popup Image" width={700} height={700} />
           </div>
         )}
+        {
+          popupContact && (
+            <div className="popup" onClick={handleCloseContact}>
+              <span className="close">&times;</span>
+              {popupContact === 'contactForm' ? <ContactUs /> : <Image src={popupContact} alt="Popup Image" width={700} height={700} />}
+            </div>
+          )
+        }
       </main>
     </>
   );
